0e8ff35b0259bdf73684b73dc272d45fdace9ca6
Rodrigo Pinheiro <rpinheir@thoughtworks.com>
2019-08-13 16:33:21 -0300

    Calculating the missing numbers

--- src/missingnumbers/MissingNumbers.kt
+++ src/missingnumbers/MissingNumbers.kt
@@ -1,7 +1,5 @@
 package missingnumbers
 
-import java.util.*
-
 fun missingNumbers(arr: Array<Int>, brr: Array<Int>): Array<Int> {
 
     val positionArray = Array<Int>(100001, {0})
@@ -17,23 +15,4 @@ fun missingNumbers(arr: Array<Int>, brr: Array<Int>): Array<Int> {
     return positionArray.indices.filter {
         i -> positionArray[i] != 0
     }.sorted().toTypedArray()
-
-
-//    val countElementsInBrr = brr.associateBy(
-//            keySelector = {e -> e},
-//            valueTransform = {e -> brr.count { p -> p ==e }}
-//    )
-//
-//    val countElementsInArr = arr.associateBy(
-//            keySelector = {e -> e},
-//            valueTransform = {e -> arr.count { p -> p ==e }}
-//    )
-//
-//    var minus = mutableListOf<Int>()
-//    countElementsInBrr.forEach {
-//        val valueInArr = countElementsInArr.getOrDefault(it.key, 0)
-//        if (valueInArr == 0 || valueInArr < it.value) minus.add(it.key)
-//    }
-//
-//    return minus.toTypedArray().sortedArray();
 }
