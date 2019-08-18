4ab01f30f652adf52db421840c18fead6ce040dd
Rodrigo Almeida <rpinheir@Rodrigos-MacBook-Pro.local>
2019-08-18 16:10:48 -0300

    Removing unnecessary code

--- src/connectedcellsingrid/ConnectedCellTest.kt
+++ src/connectedcellsingrid/ConnectedCellTest.kt
@@ -83,32 +83,4 @@ class ConnectedCellTest {
     private fun alreadySeen(seen: Array<Array<Boolean>>, posX: Int, posY: Int): Boolean {
         return seen[posX][posY]
     }
-//    fun calculateNumbersOfCells(matrix: Array<Array<Int>>, seen: Array<Array<Boolean>>, posX: Int, posY: Int): Int {
-//
-//        if (posX < 0 || posY < 0 || posX == matrix.size || posY == matrix.size) return nCells
-//
-//        if (seen[posX][posY]) return nCells
-//
-//        seen[posX][posY] = true
-//
-//        for(i in posX..matrix.size - 1) {
-//            for (j in posY..matrix[i].size - 1) {
-//                if (i < 0 || j < 0 || i == matrix.size || j == matrix.size) return nCells
-//                else if (matrix[i][j] == 1) {
-//                    nCells = nCells.inc()
-//                    calculateNumbersOfCells(matrix, seen, posX, j+1)
-//                    calculateNumbersOfCells(matrix, seen, i+1, j)
-//                    calculateNumbersOfCells(matrix, seen, i+1, j+1)
-//                    calculateNumbersOfCells(matrix, seen, i-1, j-1)
-//
-//                } else {
-//                    return nCells
-//                }
-//            }
-//            nCells = 0
-//        }
-//
-//        return nCells
-//    }
-
 }
